6902def2dcf3827eb3a9186ed4e9b8ce7e4313d1
Juan Pablo Moreno <moreno.juan@correounivalle.edu.co>
2018-02-02 12:20:42 -0500

    Se arregla el archivo javascript geographic_main.js con su respectivo .min

--- amd/src/geographic_main.js
+++ amd/src/geographic_main.js
@@ -44,83 +44,82 @@ define(['jquery', 'block_ases/bootstrap', 'block_ases/sweetalert', 'block_ases/j
 
             });
 
-            }
-        }
+                /**
+                 * @method load_geographic_info
+                 * @desc Loads all geographic info of a student given his id. Current processing on geographic_serverproc.php
+                 * @param {id} id_ases ASES student id
+                 * @return {void}
+                 */
+                function load_geographic_info(id_ases){
+                    $.ajax({
+                        type: "POST",
+                        data: {
+                            func: 'load_geographic_info',
+                            id_ases: id_ases
+                        },
+                        url: "../managers/student_profile/geographic_serverproc.php",
+                        success: function(msg) {
 
+                            console.log(msg);
+                        },
+                        dataType: "json", //Json format
+                        cache: "false",
+                        error: function(msg) {
+                            console.log(msg);
+                        },
+                    });
+                }
 
-    /**
-     * @method load_geographic_info
-     * @desc Loads all geographic info of a student given his id. Current processing on geographic_serverproc.php
-     * @param {id} id_ases ASES student id
-     * @return {void}
-     */
-    function load_geographic_info(id_ases){
-        $.ajax({
-            type: "POST",
-            data: {
-                func: 'load_geographic_info',
-                id_ases: id_ases
-            },
-            url: "../managers/student_profile/geographic_serverproc.php",
-            success: function(msg) {
+                /**
+                 * @method save_geographic_info
+                 * @desc Saves a student geographic information. Current processing on geographic_serverproc.php
+                 * @param {integer} id_ases ASES student id
+                 * @param {float} latitude latitude coordenate
+                 * @param {float} longitude longitude coordenate
+                 * @param {id} neighborhood neighborhood name
+                 * @param {id} geographic_risk geographic risk according to the neighborhood
+                 */
+                function save_geographic_info(id_ases, latitude, longitude, neighborhood, geographic_risk){
 
-                console.log(msg);
-            },
-            dataType: "json", //Json format
-            cache: "false",
-            error: function(msg) {
-                console.log(msg);
-            },
-        });
-    }
+                    $.ajax({
+                        type: "POST",
+                        data: {
+                            func: 'save_geographic_info',
+                            id_ases: id_ases,
+                            latitude: latitude,
+                            longitude: longitude,
+                            neighborhood: neighborhood,
+                            geographic_risk: geographic_risk
+                        },
+                        url: "../managers/student_profile/geographic_serverproc.php",
+                        success: function(msg) {
 
-    /**
-     * @method save_geographic_info
-     * @desc Saves a student geographic information. Current processing on geographic_serverproc.php
-     * @param {integer} id_ases ASES student id
-     * @param {float} latitude latitude coordenate
-     * @param {float} longitude longitude coordenate
-     * @param {id} neighborhood neighborhood name
-     * @param {id} geographic_risk geographic risk according to the neighborhood
-     */
-    function save_geographic_info(id_ases, latitude, longitude, neighborhood, geographic_risk){
+                            swal(
+                                msg.title,
+                                msg.text,
+                                msg.type);
+                            
+                            $('#button_edit_geographic').removeAttr('hidden');
+                            $('#div_save_buttons').attr('hidden', true);
+                            $('#select_neighborhood').attr('disabled', true);
+                            $('#select_geographic_risk').attr('disabled', true);
+                            $('#latitude').attr('disabled', true);
+                            $('#longitude').attr('disabled', true);
+                        },
+                        dataType: "json",
+                        cache: "false",
+                        error: function(msg) {
+                            
+                            swal(
+                                msg.title,
+                                msg.text,
+                                msg.type);
+                        },
+                    });
 
-        $.ajax({
-            type: "POST",
-            data: {
-                func: 'save_geographic_info',
-                id_ases: id_ases,
-                latitude: latitude,
-                longitude: longitude,
-                neighborhood: neighborhood,
-                geographic_risk: geographic_risk
-            },
-            url: "../managers/student_profile/geographic_serverproc.php",
-            success: function(msg) {
+                }
 
-                swal(
-                    msg.title,
-                    msg.text,
-                    msg.type);
-                
-                $('#button_edit_geographic').removeAttr('hidden');
-                $('#div_save_buttons').attr('hidden', true);
-                $('#select_neighborhood').attr('disabled', true);
-                $('#select_geographic_risk').attr('disabled', true);
-                $('#latitude').attr('disabled', true);
-                $('#longitude').attr('disabled', true);
-            },
-            dataType: "json",
-            cache: "false",
-            error: function(msg) {
-                
-                swal(
-                    msg.title,
-                    msg.text,
-                    msg.type);
-            },
-        });
+            }//end function
+        };//end return    
 
-    }
-
-})
+});//end of the file
